59fa448af9985d090938f1ce5d41e24f088e57f1
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2024-03-13 21:25:36 +0500

    added test class

---
+++ src/test/java/ru/netology/sqltask/test/SqlHometaskTest.java
@@ -0,0 +1,57 @@
+package ru.netology.sqltask.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.sqltask.data.DataHelper;
+import ru.netology.sqltask.data.SQLHelper;
+import ru.netology.sqltask.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.sqltask.data.SQLHelper.cleanAuthCodes;
+
+public class SqlHometaskTest {
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanAuthCodes();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Should sucessfully login to dashboard with exist login and passwordfrom sut test data")
+    void shouldSuccessfulLogin() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login with exist in base and active user and random verification code")
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! Неверно указан код! Попробуйте ещё раз.");
+    }
+}
